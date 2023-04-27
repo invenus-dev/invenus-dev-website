43005dbecf88ad08f0d7a61c0f0549a188b58168
Jan Maly <jan@invenus.dev>
2023-04-27 10:39:38 +0200

    feat: tailwind base setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from 'App';
+import './css/main.css';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./index.html', './src/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
