21606580e54b9e876fc191ae26f7655a8d891c18
Jan Maly <jan@invenus.dev>
2023-04-27 10:25:52 +0200

    feat: eslint, prettier, vite changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import App from 'App';
 
-const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  const root = createRoot(rootEl);
+  root.render(<App />);
+}
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
+// eslint-disable-next-line import/no-unused-modules
 export default {
   plugins: [react(), reactRefresh(), tsconfigPaths()],
-  build: { sourcemap: true }
 };
