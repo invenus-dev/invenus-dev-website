0ca148c9191d56593162da2812ec17efb485305c
Jan Maly <jan@invenus.dev>
2023-05-05 18:26:43 +0200

    feat: site pieces vol 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,36 @@
 import { createRoot } from 'react-dom/client';
-import App from 'App';
+import Cubicle from 'Cubicle';
 import './css/main.css';
 
-const rootEl = document.getElementById('root');
-if (rootEl) {
+// React DOM hydration
+const rootEls = Array.from(document.querySelectorAll('div.cubicle')) as HTMLDivElement[];
+rootEls.map((rootEl) => {
   const root = createRoot(rootEl);
-  root.render(<App />);
-}
+  const text = rootEl.dataset?.text;
+  root.render(<Cubicle text={text} />);
+});
+
+// Sticky enable - intersection reaction when hero is off
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('.sticky-header');
+  const triggerElement = document.getElementById('sticky-trigger');
+
+  if (header && triggerElement) {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            header.classList.remove('sticky-on');
+          } else {
+            header.classList.add('sticky-on');
+          }
+        });
+      },
+      {
+        root: null,
+        threshold: 0,
+      }
+    );
+    observer.observe(triggerElement);
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line no-undef
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 // eslint-disable-next-line no-undef
 module.exports = {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Yantramanav"', ...defaultTheme.fontFamily.sans],
+        heading: ['"Teko"', ...defaultTheme.fontFamily.sans],
+        mono: ['"Noto Sans Mono"', ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        invBlue: '#324376',
+        invPrimary: '#F5DD90',
+        invSecondary: '#FFFFFF',
+      },
+      screens: {
+        xs: '360px',
+      },
+    },
   },
   plugins: [],
 };
