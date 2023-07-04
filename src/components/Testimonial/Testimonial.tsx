e75e69817ba6b4d5ea7aa2d1bcbc4b2745d0e7fd
Jan Maly <jan@invenus.dev>
2023-07-04 10:27:57 +0200

    feat: added testimonial

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -6,7 +6,7 @@ type Props = {
 const Testimonial = ({ testimonial }: Props) => {
   const { client, name, role, body } = testimonial;
   return (
-    <div className="mb-4 flex flex-col justify-evenly gap-4 bg-gray-100 px-8 py-6 md:mb-0">
+    <div className="mb-4 flex flex-col justify-between gap-4 bg-gray-100 px-8 py-6 md:mb-0">
       <div className="italic">{body}</div>
       <div className="flex flex-wrap items-center justify-end gap-x-2 gap-y-1">
         <span className="inline-block bg-secondary px-2 py-1">
