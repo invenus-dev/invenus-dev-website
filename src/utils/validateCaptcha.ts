c28cca4df4bdd6818f94d1875cb2c82779f477c8
Honza Maly <jan@invenus.dev>
2025-11-20 11:11:07 +0100

    feat: moving to recaptcha enterprise

--- src/utils/validateCaptcha.ts
+++ src/utils/validateCaptcha.ts
@@ -2,8 +2,8 @@ const siteKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
 
 export const validateCaptcha = (): Promise<string> => {
   return new Promise((resolve) => {
-    grecaptcha.ready(() => {
-      grecaptcha.execute(siteKey, { action: 'submit' }).then((token) => {
+    grecaptcha.enterprise.ready(() => {
+      grecaptcha.enterprise.execute(siteKey, { action: 'FormSubmit' }).then((token) => {
         return resolve(token);
       });
     });
